9e98d9fb8e27c241fe8156f2eb0d0a7fb30c76aa
OlavBerg <berg_olav@yahoo.se>
2021-12-13 13:47:33 +0100

    Made h-file and c-file to random_array()

--- random_array.c
+++ random_array.c
@@ -1,9 +1,8 @@
+#include "random_array.h"
+
 #include <stdio.h> 
 #include <stdlib.h>
 #include <time.h> 
-
-
-
 
 unsigned* random_array(size_t size) {
 
@@ -20,15 +19,3 @@ unsigned* random_array(size_t size) {
 
     return array;
 }
-
-int main() {
-    int i; 
-    unsigned* array = random_array(5); 
-
-    for(i = 0; i < 5; i++) {
-        printf("%d\n", array[i]);
-    }  
-
-    return 0;
-}
- 
---
+++ random_array.h
@@ -0,0 +1,8 @@
+#ifndef RANDOM_ARRAY_H
+#define RANDOM_ARRAY_H
+
+#include <stdlib.h>
+
+unsigned* random_array(size_t size);
+
+#endif // #ifndef RANDOM_ARRAY_H
