c967c8ae4410c1234e6addd1af7ba7ea8a8bb5fa
Oscar Tideback <tideback@gmail.com>
2021-12-15 15:42:14 +0100

    Save file

--- main.c
+++ main.c
@@ -2,14 +2,11 @@
 #include <stdlib.h>
 #include <stdbool.h> 
 #include <time.h> 
+#include <string.h>
 #include "menu.h"
 #include "quicksort.h"
 #include "heapsort.h"
 #include "random_array.h"
-
-typedef struct List{
-  char *array[16];
-} list;
 
 void printArray(unsigned *arr, size_t size);
 
@@ -19,6 +16,9 @@ static void sorting_test(unsigned* (radnomizer)(size_t), void (algorithm)(unsign
   double cpu_time_used;
   char str_arr[64][32];
   unsigned* arr = radnomizer(size_of_first_test); 
+  if (algorithm == quicksort) { filename = "quicksort"; }
+  else if(algorithm == heapSort){ filename = "heapsort"; }
+  /*else if(algorithm == bublesort){filename = "bublesort";}*/
   //printArray(arr, size_of_first_test);
   for(int i = 0; i < num_of_tests; i++){
     if(i != 0){// Räkna bort första körning
@@ -28,25 +28,25 @@ static void sorting_test(unsigned* (radnomizer)(size_t), void (algorithm)(unsign
     start = clock();
     algorithm(arr, size_of_first_test);
     end = clock();
+
     if(i != 0){
-      //int result = end-start;
-      sprintf(*str_arr+i, "%i, %i",i , (end-start));
-      printf("%s\n", *str_arr+i);
+      int value = (end-start), results, element = size_of_first_test/(num_of_tests-1);
+      int length = snprintf( NULL, 0, "%d", value );
+      char* valueAsString = malloc( length + 1 );
+      snprintf( valueAsString, length + 1, "%d", value );
+      strcpy(&str_arr[i][0], valueAsString);
+      FILE *file = fopen(filename, "w");
+      for(int i = 1; i < num_of_tests; i++){
+        printf("\n");
+        results = fprintf(file, "%i, %s\n", element * i , str_arr+i);
+        printf("%s\n", str_arr+i);
+      }
+      if (results == EOF) {
+      printf("Fail...");
+      }
+      fclose(file);
     }
-    //cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    //printf("Size:(%d) Start: %d End: %d: CPU Time: %d, Time: %d\n",size, start, end, cpu_time_used, (end-start));
-    }
-    //printArray(arr, size_of_first_test);
-    if (algorithm == quicksort) {
-      filename = "quicksort";
-    }
-    else if(algorithm == heapSort){
-      filename = "heapsort";
-    }
-    /*else if(algorithm == bublesort){
-      filename = "bublesort";
-    }*/
-    saveFile(str_arr, filename, num_of_tests);
+  }
   free(arr);  
 }
 
@@ -56,23 +56,9 @@ void printArray(unsigned *arr, size_t size){
   }
 }
 
-void saveFile(char *str_arr, const char *filename, int antal){
-  //FILE *file = fopen(filename, "w");
-  //int results = fputs(str_arr, file);
-  for(int i = 1; i < antal; i++){
-    printf("Save function\n");
-    //fprintf(file,"%s\n", *str_arr+x);
-    printf("%s\n", str_arr+i);
-  }
-  /*if (results == EOF) {
-      printf("Fail...");
-  }
-  fclose(file);*/
-}
-
 int main(int argc, char *argv[]){
   printf("Heapsort:\n");
-	sorting_test(shuffle, heapSort,  4, 500, 500000, "file_name");
+	sorting_test(shuffle, heapSort,  4, 0, 500000, "file_name");
  // printf("Quicksort:\n");
   //sorting_test(shuffle, quicksort,  11, 500, 500000, "file_name");
   while(true){
