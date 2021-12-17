6139776d8a7ddf6066ed68dabab48dcc358aac47
OlavBerg <berg_olav@yahoo.se>
2021-12-17 14:51:32 +0100

    .

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "heapsort.h"
 #include "quicksort.h"
 #include "heapsort.h"
+#include "bubblesort.h"
 #include "sorting_test.h"
 
 #include <stdio.h>
@@ -21,17 +22,21 @@ int main (void) {
     
     size_t n0 = 100000;
     size_t size_N = 10;
-    //const char *filename = "test.txt";
+    unsigned* (*randomizer)(size_t) = random_array;
 
     size_t N[size_N];
     for (size_t i = 0, n = n0; i < size_N; i++, n <<= 1) {
         N[i] = n;
     }
 
-    //size_t N[3] = {10, 20, 30};
+    // Quicksort
+    //sorting_test(randomizer, quicksort, N, size_N, "quicksort.txt");
 
-    sorting_test(random_array, quicksort, N, size_N, "quicksort.txt");
-    sorting_test(random_array, heapsort, N, size_N, "heapsort.txt");
+    // Heapsort
+    //sorting_test(randomizer, heapsort, N, size_N, "heapsort.txt");
+
+    // Bubblesort
+    sorting_test(randomizer, bubblesort, N, size_N, "bubblesort.txt");
 
     return 0;
 }  
