4950dedf20dc29903dea626600e77ec9bdbb27bf
OlavBerg <berg_olav@yahoo.se>
2021-12-18 15:26:08 +0100

    is_sorted is now a part of the "help_functions" module.

--- help_functions.c
+++ help_functions.c
@@ -1,5 +1,7 @@
 #include "help_functions.h"
 
+#include <stdbool.h>
+
 // Swaps elements A[i] and A[j];
 void swap(unsigned *A, int i, int j) {
     unsigned tmp = A[i];
@@ -12,3 +14,11 @@ void printArray(unsigned *arr, size_t size){
     printf("%u ", arr[i]);
   }
 }
+
+static bool is_sorted (unsigned *A, size_t n) {
+    for (int i = 0; i < n - 1; i++) {
+        if (A[i] > A[i + 1]) {
+            return false;
+        }
+    }
+    return true;
